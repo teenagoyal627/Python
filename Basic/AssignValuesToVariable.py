1d3f40cf6348551dc244f229720df6f767d37a8d
teenagoyal627 <teenagoyal627@gmail.com>
2022-08-31 21:46:09 +0530

    Programs of python

---
+++ Basic/AssignValuesToVariable.py
@@ -0,0 +1,16 @@
+a,b=2,3
+print(a)
+print(b)
+# easily run
+a,b,c=2,3
+print(a)
+print(b)
+# it gives error because c isme extra h isko koi bhi value assign nahi ho rahi h
+a,b=3,4,5
+print(a)
+print(b)
+# error because two variable and 3 values are assign
+fname,mname,lname="Nirmal","pratap","singh"
+print(fname,mname,lname)
+street,*address,country=289,"kotputli","jaipur","india"
+print(street,address,country)
---
+++ Basic/howrunProg/Ans.py
@@ -0,0 +1,2 @@
+# to run the programs of python we write 
+# python3 program name.py
