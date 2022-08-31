1f7fdd96695429d27563c72c60e4689e2055a121
teenagoyal627 <teenagoyal627@gmail.com>
2022-08-31 21:54:42 +0530

    write some comments

--- Basic/AssignValuesToVariable.py
+++ Basic/AssignValuesToVariable.py
@@ -5,7 +5,7 @@ print(b)
 a,b,c=2,3
 print(a)
 print(b)
-# it gives error because c isme extra h isko koi bhi value assign nahi ho rahi h
+# it gives error because c isme extra h isko koi bhi value assign nahi ho rahi h/
 a,b=3,4,5
 print(a)
 print(b)
